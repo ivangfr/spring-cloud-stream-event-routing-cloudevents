b52c6467bb4a47d60bf7a499b479fe570b648534
Ivan Franchin <ivan.franchin@takeaway.com>
2021-09-03 22:03:57 +0100

    add native build

--- consumer-service/src/main/java/com/mycompany/consumerservice/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/com/mycompany/consumerservice/ConsumerServiceApplication.java
@@ -1,8 +1,22 @@
 package com.mycompany.consumerservice;
 
+import com.mycompany.consumerservice.kafka.alert.event.EarthquakeAlert;
+import com.mycompany.consumerservice.kafka.alert.event.WeatherAlert;
+import com.mycompany.consumerservice.kafka.news.event.CNNNewsCreated;
+import com.mycompany.consumerservice.kafka.news.event.DWNewsCreated;
+import com.mycompany.consumerservice.kafka.news.event.RAINewsCreated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
+import java.net.URI;
+
+@TypeHint(
+        types = {
+                EarthquakeAlert.class, WeatherAlert.class,
+                CNNNewsCreated.class, DWNewsCreated.class, RAINewsCreated.class,
+                URI.class
+        })
 @SpringBootApplication
 public class ConsumerServiceApplication {
 
--- producer-service/src/main/java/com/mycompany/producerservice/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/mycompany/producerservice/ProducerServiceApplication.java
@@ -1,8 +1,22 @@
 package com.mycompany.producerservice;
 
+import com.mycompany.producerservice.kafka.alert.event.EarthquakeAlert;
+import com.mycompany.producerservice.kafka.alert.event.WeatherAlert;
+import com.mycompany.producerservice.kafka.news.event.CNNNewsCreated;
+import com.mycompany.producerservice.kafka.news.event.DWNewsCreated;
+import com.mycompany.producerservice.kafka.news.event.RAINewsCreated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 
+import java.util.LinkedHashMap;
+
+@TypeHint(
+        types = {
+                EarthquakeAlert.class, WeatherAlert.class,
+                CNNNewsCreated.class, DWNewsCreated.class, RAINewsCreated.class,
+                LinkedHashMap.class
+        })
 @SpringBootApplication
 public class ProducerServiceApplication {
 
