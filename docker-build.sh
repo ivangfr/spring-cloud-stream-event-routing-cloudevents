3c0a1f4c3a23a22936c862399540c6878a9011da
Ivan Franchin <ivan.franchin@takeaway.com>
2024-11-01 13:27:35 +0100

    Replace Jib with Paketo Buildpacks

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_VERSION="1.0.0"
+
+PRODUCER_APP_NAME="producer-service"
+CONSUMER_APP_NAME="consumer-service"
+PRODUCER_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${PRODUCER_APP_NAME}:${APP_VERSION}"
+CONSUMER_DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${CONSUMER_APP_NAME}:${APP_VERSION}"
+
+SKIP_TESTS="true"
+
 if [ "$1" = "native" ];
 then
-  ./mvnw clean -Pnative spring-boot:build-image --projects producer-service -DskipTests
-  ./mvnw clean -Pnative spring-boot:build-image --projects consumer-service -DskipTests
+  ./mvnw -Pnative clean spring-boot:build-image --projects "$PRODUCER_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$PRODUCER_DOCKER_IMAGE_NAME"
+#  ./mvnw -Pnative clean spring-boot:build-image --projects "$CONSUMER_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$CONSUMER_DOCKER_IMAGE_NAME"
 else
-  ./mvnw clean compile jib:dockerBuild --projects producer-service
-  ./mvnw clean compile jib:dockerBuild --projects consumer-service
+  ./mvnw clean spring-boot:build-image --projects "$PRODUCER_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$PRODUCER_DOCKER_IMAGE_NAME"
+  ./mvnw clean spring-boot:build-image --projects "$CONSUMER_APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$CONSUMER_DOCKER_IMAGE_NAME"
 fi
