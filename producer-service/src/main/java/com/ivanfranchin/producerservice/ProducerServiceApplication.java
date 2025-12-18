b8539d3a587c14dbde4014009413eee0fe0470d4
ivangfr <ivangfr@yahoo.com.br>
2025-12-18 18:36:48 -0300

    Project upgrade
    - Upgrade to Spring Boot 3.5.9
    - Upgrade to Spring Cloud 2025.0.1
    - Upgrade to wrapperVersion to 3.3.4 and Apache Maven to 3.9.12
    - Upgrade to Confluentinc Docker image 7.9.5
    - Upgrade to Kafdrop Docker image 4.2.0
    - Refactor pom.xml files
    - Remove NativeRuntimeHintsRegistrar as it is not needed

--- producer-service/src/main/java/com/ivanfranchin/producerservice/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/ivanfranchin/producerservice/ProducerServiceApplication.java
@@ -1,11 +1,8 @@
 package com.ivanfranchin.producerservice;
 
-import com.ivanfranchin.producerservice.config.NativeRuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
-@ImportRuntimeHints(NativeRuntimeHintsRegistrar.class)
 @SpringBootApplication
 public class ProducerServiceApplication {
 
